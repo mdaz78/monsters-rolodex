f22c2a496ea8041f63a4f22464be04a8dbe4b07a
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-12-28 14:38:38 +0530

    Style the App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import './App.css';
+
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 
@@ -28,7 +30,8 @@ class App extends Component {
     );
 
     return (
-      <div style={{ textAlign: 'center' }}>
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={this.handleChange}
