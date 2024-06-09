03aaa9e3498beae45c4da5bc397dcba7f5f74b28
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-09 11:10:09 +0530

    memoize card component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,16 +1,19 @@
+import { memo } from "react";
 import "./card.styles.css";
 
-const Card = ({ name, id, email }) => (
-  <div className="card-container">
-    <img
-      src={`https://robohash.org/${id}?setset2&size=180x180`}
-      alt={`monster ${name}`}
-      width={"180px"}
-      height={"180px"}
-    />
-    <h2>{name}</h2>
-    <p>{email}</p>
-  </div>
-);
+const Card = ({ name, id, email }) => {
+  return (
+    <div className="card-container">
+      <img
+        src={`https://robohash.org/${id}?setset2&size=180x180`}
+        alt={`monster ${name}`}
+        width={"180px"}
+        height={"180px"}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
-export default Card;
+export default memo(Card);
