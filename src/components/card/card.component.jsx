630e16439945a0946c5f7a450dcfb0f02705f2eb
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-09 11:08:10 +0530

    minor perf improvements for CLS

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,6 +5,8 @@ const Card = ({ name, id, email }) => (
     <img
       src={`https://robohash.org/${id}?setset2&size=180x180`}
       alt={`monster ${name}`}
+      width={"180px"}
+      height={"180px"}
     />
     <h2>{name}</h2>
     <p>{email}</p>
